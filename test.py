44e180523e01a7ac2f803078b3048836edc6766e
Paul Lee <enigma@enigmatic.network>
2019-05-22 19:44:45 -0400

    get rid of test

--- test.py
+++
@@ -1,9 +0,0 @@
-import database as Database
-
-me = Database("users", "hillcrestpaul0719")
-try:
-    print(me.variables['password'])
-except:
-    pass
-me.variables['password'] = "newPassword"
-me.save()
